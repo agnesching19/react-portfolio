1f89bcfc7594f36c709992dea9a2f9e8cb8e1f7f
Agnes Ching <agnesching19@gmail.com>
2018-01-05 21:04:26 +0000

    arrays in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,6 +29,8 @@ var onRemoveAll = function onRemoveAll() {
 
 var appRoot = document.getElementById('app');
 
+var numbers = [55, 101, 1000];
+
 var render = function render() {
   var template = React.createElement(
     'div',
@@ -61,17 +63,13 @@ var render = function render() {
     React.createElement(
       'ol',
       null,
-      React.createElement(
-        'li',
-        null,
-        'Item one'
-      ),
-      React.createElement(
-        'li',
-        null,
-        'Item two'
-      ),
-      '.'
+      app.options.map(function (op) {
+        return React.createElement(
+          'li',
+          { key: op },
+          op
+        );
+      })
     ),
     React.createElement(
       'form',
--- src/app.js
+++ src/app.js
@@ -27,6 +27,8 @@ const onRemoveAll = () => {
 
 const appRoot = document.getElementById('app');
 
+const numbers = [55, 101, 1000];
+
 const render = () => {
   const template = (
     <div>
@@ -35,10 +37,13 @@ const render = () => {
       <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
       <p>{app.options.length}</p>
       <button onClick={onRemoveAll}>Remove All</button>
+
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>.
+        {
+          app.options.map((op) => <li key={op}>{op}</li>)
+        }
       </ol>
+
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option"/>
         <button>Add Option</button>
