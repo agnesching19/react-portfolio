0c3263c21aef7fcc2b1bbebbff24e167ba5d29e7
Agnes Ching <agnesching19@gmail.com>
2018-01-05 19:49:56 +0000

    data binding

--- src/app.js
+++ src/app.js
@@ -23,28 +23,33 @@ const template = (
 let count = 0;
 
 const addOne = () => {
-  console.log('addOne');
+  count ++;
+  renderCounterApp();
 };
 
 const minusOne = () => {
-  console.log('minusOne');
+  count --;
+  renderCounterApp();
 };
 
 const reset = () => {
-  console.log('reset');
+  count = 0;
+  renderCounterApp();
 };
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
-
-console.log(templateTwo);
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
