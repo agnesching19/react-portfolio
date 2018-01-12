b0453e402add3010dc0440eddd0da9bd8b06fdad
Agnes Ching <agnesching@agnesching.local>
2018-01-12 17:10:28 +0000

    imported npm modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const template = React.createElement('p', {}, 'bello bello');
+ReactDom.render(template, document.getElementById('app'));
+
 // import './utils.js';
 // import substract, { square, add } from './utils.js';
 
@@ -7,8 +13,8 @@
 // console.log(add(100, 55));
 // console.log(substract(18, 4));
 
-import isSenior, { isAdult, canDrink } from './person.js';
+// import isSenior, { isAdult, canDrink } from './person.js';
 
-console.log(isAdult(20));
-console.log(canDrink(17));
-console.log(isSenior(70));
+// console.log(isAdult(20));
+// console.log(canDrink(17));
+// console.log(isSenior(70));
