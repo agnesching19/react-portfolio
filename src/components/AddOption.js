221cced5eaa2a0668203db98934d2ebe6aebceb6
Agnes Ching <agnesching@agnesching.local>
2018-01-12 21:25:32 +0000

    es6 class properties

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
+  state = {
+    error: undefined
   };
-  handleAddOption(e) {
+
+  handleAddOption = (e) => {
     e.preventDefault();
 
     const option = e.target.elements.option.value.trim();
@@ -19,7 +16,8 @@ export default class AddOption extends React.Component {
     if (!error) {
       e.target.elements.option.value = '';
     }
-  }
+  };
+
   render() {
     return (
       <div>
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,52 +5,24 @@ import Header from './Header.js';
 import Action from './Action.js';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('options');
-      const options = JSON.parse(json);
+  state = {
+    options: []
+  };
 
-      if (options) {
-        this.setState(() => ({ options }));
-      }
-    } catch (e) {
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      console.log('saving date');
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
-    }
-    console.log('saving data');
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
-  }
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   }
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option)
     }));
   }
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
       return 'Enter valid value to add items';
     } else if (this.state.options.indexOf(option) > -1) {
@@ -79,5 +51,28 @@ export default class IndecisionApp extends React.Component {
         />
       </div>
     );
+  }
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      console.log('saving date');
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+    console.log('saving data');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
   }
 };
