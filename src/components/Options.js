bab79390822310fb726044c37175a9a144a6cd41
Agnes Ching <agnesching@agnesching.local>
2018-01-13 13:17:42 +0000

    refactored stateless functional components

--- src/components/Options.js
+++ src/components/Options.js
@@ -14,22 +14,20 @@ import Option from './Option.js';
 //   }
 // }
 
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
-      {
-        props.options.map((option) => (
-          <Option
-            key={option}
-            optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {
+      props.options.map((option) => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
 
 export default Options;
