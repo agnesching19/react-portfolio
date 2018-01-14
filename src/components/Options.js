87415bf1234d3d4b0c332667b80cfca10bc6aa95
Agnes Ching <my email address used on my git repo>
2018-01-14 14:29:19 +0000

    big button and options list

--- src/components/Options.js
+++ src/components/Options.js
@@ -16,7 +16,12 @@ import Option from './Option.js';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <button
+      onClick={props.handleDeleteOptions}
+      className="button button--link"
+    >
+      Remove All
+    </button>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
     {
       props.options.map((option) => (
