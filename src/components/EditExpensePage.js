009ef94eef8b65cf0d9b84def8d53f8505337527
Agnes Ching <agnesching19@gmail.com>
2018-01-17 18:52:32 +0000

    query strings and URL parameters

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from the edit expense page
-  </div>
-);
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      editing the expense with id of {props.match.params.id}
+    </div>
+  );
+};
 
 export default EditExpensePage;
